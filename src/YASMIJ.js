ad1dc9f86b9715aadd127b658e61d85ed0f292f0
Larry Battle <http://bateru.com/news/contact-me>
2012-06-24 22:50:19 -0700

    add and deleted files.

--- src/YASMIJ.js
+++ src/YASMIJ.js
@@ -1,21 +1,128 @@
 ﻿/*
 * @project YASMIJ.js, "Yet another simplex method implementation in Javascript"
-* @note The simplex method will solve max and min optimization for Linear Programming problems by use of artifical variables and branch and bound.
-* @info Project page: <https://github.com/LarryBattle/YASMIJ.js/>
-* @author Larry Battle <http://bateru.com/news>
-* @date May 30, 2012
-* @version Beta 0.0.1
+*/
+// Equation Class
+var Equation = function(){
+	this.comparison;
+	this.leftSide;
+	this.rightSide;
+	this.terms;
+};
+Equation.hasManyCompares = function(str){
+	var RE_compares = /[<>]=?|=/g;
+	var matches = (""+str).replace(/\s/g,"").match(RE_compares) || [];
+	return 1 < matches.length;
+};
+Equation.hasOtherMathSigns = function( str ){
+	return /[\*\/%]/.test(str);
+};
+Equation.hasIncompleteBinaryOperator = function( str ){
+	var noSpaceStr = (""+str).replace( /\s/g, "" );
+	var RE_hasNoPlusOrMinus = /^[^\+\-\d]+$/, RE_noLeftAndRightTerms = /[\+\-][><=\+\-]|[><=\+\-]$/;	
+	return RE_hasNoPlusOrMinus.test(noSpaceStr) || RE_noLeftAndRightTerms.test(noSpaceStr);
+};
+Equation.getErrorMessage = function( str ){
+	var errMsg;
+	if( Equation.hasManyCompares( str ) ){
+		errMsg = "Only 1 comparision (<,>,=, >=, <=) is allow in a equation.";
+	}
+	if( !errMsg && Equation.hasOtherMathSigns( str ) ){
+		errMsg = "Only addition and subtraction is allowed. Get rid of math operators.";
+	}
+	if( !errMsg && Equation.hasIncompleteBinaryOperator( str ) ){
+		errMsg = "Math operators must be in between terms. Good:(a+b=c). Bad:(a b+=c)";
+	}
+	return errMsg;
+};
+Equation.checkInput = function( str ){
+	var errMsg = Equation.getErrorMessage( str );
+	if( errMsg ){
+		throw new Error( errMsg );
+	}
+};
+Equation.parse = function( str ){
+	Equation.checkInput( str );
+	if( !str ){
+		return {lhs:{0:1}};
+	}
+	if( !isNaN( str ) ){
+		return { lhs: {1: parseFloat(str) } }
+	}
+};
+/*
+// Simplex Class
+var Simplex = function(){
+	this.input = new Input();
+	this.output = new Output();
+	this.tableau = new Tableau();
+	this.state;
+};
+Simplex.prototype = {
+	toString:function(){
+	},
+	setInput:function( obj ){
+		this.input.parse( obj );
+	}
+};
+// Input Class
+var Input = function(){
+	this.z;
+	this.raw;
+	this.type;
+	this.variables;
+	this.constraints;
+};
+Input.checkForRequirements = function( obj ){
+	var errMsg = "Input Object";
+	if( !obj || typeof obj !== "object" ){
+		errMsg = "must be an object with properies `z`, `constraints` and `type`.";
+	}
+	return errMsg;
+}
+Input.prototype = {
+	toString:function(){
+	},
+	parse:function( obj ){
+		this.raw = obj;
+		var zEquation = new Equation(obj.z);
+		this.z = obj.z;
+	}
+};
+// Matrix Class
+var Matrix = function(){
+	this.columnLength;
+	this.rowLength;
+	this.matrix;
+};
+// Tableau Class
+var Tableau = function(){
+	this.matrix;
+	this.limit;
+	this.state;
+};
+
+// Output Class
+var Output = function(){
+	this.matrix;
+	this.result;
+};
+Output.prototype = {
+	toString:function(){
+	}
+};
 */
 
-// Todo: Find out how to model the equations.
-// Todo: Find out if underscore.coffee will be of good use.
 
-var Simplex = function(){
-	this.equations = [];
-	this.interationLimit = 1e5;
-};
-Simplex.prototype.getResults = function(){
-	return false;
-};
 
-var YASMIJ = Simplex;
+
+
+
+
+
+
+
+
+
+
+
+
--- tests/js/YASMIJ-testcases.js
+++ tests/js/YASMIJ-testcases.js
@@ -1,16 +1,175 @@
 ﻿/**
 * @project YASMIJ.js
-* @purpose Testcases for new YASMIJ.js
-* @author Larry Battle , <http://bateru.com/news/>
-* @date May 30, 2012
-* @license MIT and GPL 3.0 
-* MIT License <http://www.opensource.org/licenses/mit-license>
-* GPL v3 <http://opensource.org/licenses/GPL-3.0>
-* @info Project page: <https://github.com/LarryBattle/YASMIJ.js/>
-* @version Beta 0.0.1, 2012.05.30
+* @requires YASMIJ.js, Sinon.js, jQuery.js
 */
 $(function(){
-	test( "test Simplex for existance", function(){
-		ok( Simplex, "My first test." );
+
+	module( "Equation Class" );
+	test( "test Equation.hasManyCompares() with 1 compare", function(){
+		var func = Equation.hasManyCompares;
+		equal( func( "a < b" ), false );
+		equal( func( "a > b" ) , false );
+		equal( func( "a = b" ) , false );
+		
+		equal( func( "a >= b" ) , false );
+		equal( func( "a <= b" ) , false );
+		
+		equal( func( "a = < b" ), true );
+		equal( func( "a => b" ), true );
 	});
+	test( "test Equation.hasManyCompares() with many compares", function(){
+		var func = Equation.hasManyCompares;
+		equal( func( "a << b" ), true );
+		
+		equal( func( "a < < b" ), true );
+		equal( func( "a >> b" ), true );
+		equal( func( "a > > b" ), true );
+		
+		equal( func( "a == b" ), true );
+		equal( func( "a = = b" ), true );
+		equal( func( "a >=>= b" ), true );
+		
+		equal( func( "a <=<= b" ), true );
+		equal( func( "a > b > c" ), true );
+		equal( func( "a > b < c" ), true );
+		
+		equal( func( "a < b < c" ), true );
+	});
+	test( "test Equation.hasOtherMathSigns() with valid expressions.", function(){
+		var func = Equation.hasOtherMathSigns;
+		
+		equal( func( "" ), false );
+		equal( func( "-a" ), false );
+		equal( func( "+a" ), false );
+		equal( func( "+a - b" ), false );
+		equal( func( "a + b" ), false );
+	});
+	test( "test Equation.hasOtherMathSigns() with invalid expressions.", function(){
+		var func = Equation.hasOtherMathSigns;
+		
+		equal( func( "a * b" ), true );
+		equal( func( "a / b + c" ), true );
+		equal( func( "a % b" ), true );
+	});
+	test( "test Equation.hasIncompleteBinaryOperator() with valid expressions.", function(){
+		var func = Equation.hasIncompleteBinaryOperator;
+		
+		equal( func( "" ), false );
+		equal( func( "-1" ), false );
+		equal( func( "1" ), false );
+		equal( func( "+1" ), false );
+		equal( func( "+1003" ), false );
+		equal( func( " +13.23 " ), false );
+		equal( func( "-a" ), false );
+		equal( func( "+a" ), false );
+		equal( func( "a" ), false );
+		equal( func( "+a - b" ), false );
+		equal( func( "a + b" ), false );
+	});
+	test( "test Equation.hasIncompleteBinaryOperator() with invalid expressions.", function(){
+		var func = Equation.hasIncompleteBinaryOperator;
+		
+		equal( func( "a b" ), true );
+		equal( func( "a < b c" ), true );
+		equal( func( "a-" ), true );
+		equal( func( "a+" ), true );
+		equal( func( "a+ < c-" ), true );
+		equal( func( "a+ b -= c" ), true );
+		equal( func( "a+ b += c" ), true );
+	});
+	test( "test that Equation.getErrorMessage() ", function(){	
+		var func = Equation.getErrorMessage;
+		ok( !func( "a+b" ) );
+		ok( func( "a b" ) );
+		ok( func( "a * b" ) );
+		ok( func( "a + b+" ) );
+	});
+	test( "test that Equation.checkInput calls Equation.getErrorMessage", function(){	
+		var stub = sinon.stub( Equation, 'getErrorMessage' );
+		Equation.checkInput();
+		ok( stub.called );
+	});
+	test( "test that Equation.parse calls Equation.checkInput ", function(){	
+		var stub = sinon.stub( Equation, 'checkInput' );
+		Equation.parse();
+		ok( stub.called );
+	});
+	test( "test Equation.parse() for single number", function(){
+		var func = Equation.parse;
+		deepEqual(func( "" ), {lhs:{0:1}} );
+		deepEqual(func( "1" ), {lhs:{1:1}} );
+		deepEqual(func( "-20" ), {lhs:{1:-20}} );
+		deepEqual(func( "-423.12345" ), {lhs:{1:-423.12345}} );
+		deepEqual(func( "-1.23e-34" ), {lhs:{1:-1.23e-34}} );
+	});
+	test( "test Equation.parse() for single variables", function(){
+		var func = Equation.parse;
+		deepEqual(func( "x" ), {lhs:{x:1}} );
+		deepEqual(func( "-x" ), {lhs:{x:-1}} );
+		deepEqual(func( "+x" ), {lhs:{x:1}} );
+		deepEqual(func( "1x" ), {lhs:{x:1}} );
+		deepEqual(func( "-1x" ), {lhs:{x:-1}} );
+		deepEqual(func( "-2x" ), {lhs:{x:-2}} );
+		deepEqual(func( "-20.2x" ), {lhs:{x:-20.2}} );
+		deepEqual(func( "-20.000042x" ), {lhs:{x:-20.000042}} );
+		deepEqual(func( "-20.34ex" ), {lhs:{x:-20e34}} );
+		deepEqual(func( "-1.23e-34x" ), {lhs:{x:-1.23e-34}} );
+	});
+	
+	
+	
+	
+	
+	
+	
+	/*
+	module( "Input Class" );
+	test( "", function(){
+		ok(1);
+	});
+	module( "Matrix Class" );
+	test( "", function(){
+		ok(1);
+	});
+	module( "Tableau Class" );
+	test( "", function(){
+		ok(1);
+	});
+	module( "Output Class" );
+	test( "", function(){
+		ok(1);
+	});
+	module( "Simplex Class" );
+	test( "", function(){
+		ok(1);
+	});
+	module( "Use Cases" );
+	test( "test setting the simplex input", function(){
+		var input = {};
+		input.goal = "maximize";
+		input.z = "x1+2x2-x3";
+		input.constraints = [
+			"2x1 + x2 + x3 <= 14",
+			"4x1 + 2x2 + 3x3 <= 28",
+			"2x1 + 5x2 + 5x3 <= 30"
+		];
+		var problem = new Simplex();
+		problem.setInput( input );
+		
+		equal( problem.input.raw.z, input.z );
+		deepEqual( problem.input.raw.constraints, input.constraints );
+		
+		// deepEqual( problem.input.z.valueOf(), [1,2,-1] );
+		// deepEqual( problem.input.constraints.valueOf(), [
+			// [2,1,1,"<=",14],[4,2,3,"<=",28],[2,5,5,"<=",30]
+		// ] );
+		// deepEqual( problem.input.constraints, input.constraints );
+	});
+	test( "", function(){
+		ok(1);
+	});
+	test( "", function(){
+		ok(1);
+	});
+	*/
 });
