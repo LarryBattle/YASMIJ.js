6bcfd4878966c80ccfa905ec3c59f30f14d5f99f
Larry Battle <larry.battle.tech@gmail.com>
2012-08-29 17:16:49 -0500

    Update src/dev/Input.js

--- src/dev/Input.js
+++ src/dev/Input.js
@@ -14,7 +14,6 @@
  */
 var Input = function(){
 	this.z = null;
-	this.raw = null;
 	this.type = null;
 	this.terms = null;
 	this.constraints = null;
@@ -58,6 +57,18 @@ Input.prototype.convertConstraintsToStandardForm = function(){
  * @returns {Object}
  * @example
  */
+Input.prototype.addNumbersToSlacks = function(){
+	var c = this.constraints;
+	var slackI = 1;
+	for( var i = 0, len = c.length; i < len; i++){
+		if( c[ i ].slackValue ){
+			value = c[ i ].leftSide.getTermValue( "slack" );
+			c[ i ].leftSide.removeTerm( "slack" );
+			c[ i ].leftSide.addTerm( "slack"+slackI, value );
+			slackI++;
+		}
+	}
+};
 Input.prototype.setTermNames = function(){
 	var vars = [], i = this.constraints.length, Constraint;
 	while( i-- ){
@@ -79,7 +90,7 @@ Input.prototype.getErrors = function(){
 		errMsgs.push( "z must be a string." );
 	}
 	if( this.type !== "maximize" && this.type !== "minimize" ){
-		errMsgs.push( "Maximize and minimize are the only types that is currently supported." );
+		errMsgs.push( "`maximize` and `minimize` are the only types that is currently supported." );
 	}
 	if( "[object Array]" !== Object.prototype.toString.call( this.constraints ) || !this.constraints ){
 		errMsgs.push( "Constraints must be an array with at least one element." );
@@ -119,6 +130,12 @@ Input.prototype.toString = function(){
  * @returns {Object}
  * @example
  */
+Input.prototype.convertConstraintsToMaxForm = function(){
+	var c = this.constraints;
+	for( var i = 0, len = c.length; i < len; i++){
+		c[i] = c[i].getStandardMaxForm();
+	}
+};
 Input.prototype.convertToStandardForm = function(){
 	if( this.isStandardMode ){
 		return this;
@@ -126,6 +143,9 @@ Input.prototype.convertToStandardForm = function(){
 	if( /min/.test(this.type) ){
 		this.z = this.z.inverse();
 	}
+	this.convertConstraintsToMaxForm();
+	this.addNumbersToSlacks();
+	this.setTermNames();
 	this.isStandardMode = true;
 	return this;
 };
