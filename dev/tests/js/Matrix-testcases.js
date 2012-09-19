a867ac20a2a6b6936039e1208c82c5c6a9425bea
Larry Battle <larry.battle.tech@gmail.com>
2012-09-19 10:50:06 -0500

    Update dev/tests/js/Matrix-testcases.js
    Added extra test case for bug fix for Matrix.prototype.getRowIndexWithPosMinColumnRatio

--- dev/tests/js/Matrix-testcases.js
+++ dev/tests/js/Matrix-testcases.js
@@ -144,6 +144,7 @@ tests.runMatrixTests = function(){
 		equal( func([[-2,1],[-1,2]], 0), -1 );
 		equal( func([[1,1],[2,1],[3,2]], 0), 1 );
 		equal( func([[1,1],[1,2],[1,0.1]], 0), 0 );
+		equal( func([[1,0,1,1,4],[0,1,2,1,5],[0,0,-3,-4,0]], 3), 0 );
 	});
 	test( "test Matrix.prototype.pivot()", function(){
 		var func = function( arr, pRow, pCol ){
