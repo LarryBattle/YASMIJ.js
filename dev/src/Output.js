e59d5e58a334c8ed33bbd915e1e52f6f2f7fe375
Larry Battle <larry.battle.tech@gmail.com>
2012-11-19 13:11:13 -0600

    Update dev/src/Output.js

--- dev/src/Output.js
+++ dev/src/Output.js
@@ -8,13 +8,44 @@
 * @todo Use create a fuction for objectToString since JSON.stringify isn't supported by older browsers.
 */
 // Output Class
-var Output = function(){
-	this.matrix = null;
-	this.result = null;
+var Output = function(obj, matrix){
+	this.matrix = matrix;
+	this.result = obj;
+	this.checkForError();
+	return this;
+};
+Output.getErrorMessage = function(obj, matrix){
+	var errMsg;
+	if(typeof obj !== "object"){
+		errMsg = "An object must be passed.";
+	}else if(!(matrix instanceof Matrix)){
+		errMsg = "Must pass an instance of matrix.";
+	}
+	return errMsg;
+};
+Output.parse = function(obj, matrix){
+	return new Output(obj, matrix);
+};
+Output.prototype.checkForError = function(){
+	var errMsg = Output.getErrorMessage(this.result, this.matrix);
+	if(errMsg){
+		throw new Error(errMsg);
+	}
 };
 Output.prototype.toString = function(){
-	var str = "";
-	str += "matrix = " + ( this.matrix instanceof Matrix ? this.matrix.toString() : [] );
-	str += "\n result = " + JSON.stringify(result);
-	return str;
+	return JSON.stringify(this);
+};
+Output.prototype.convertToRatio = function(){
+	if(!Ratio){
+		throw new Error( "Ratio.js is required." );
+	}
+	a.matrix.forEachRow(function(i, row, rows){
+		for(var i = 0, len = row.length; i < len; i++){
+			row[i] = Ratio.parse( row[i] ).reduce().toLocaleString();
+		}
+	});
+	for( v in a.result ){
+		a.result[v] = Ratio(a.result[v]).reduce().toLocaleString();
+	}
+	return this;
 };
