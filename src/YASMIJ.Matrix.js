e8f73ad418caa6dd971c415fab921104084bba6e
Larry Battle <larry.battle.tech@gmail.com>
2013-06-13 17:48:10 -0500

    Update YASMIJ.Matrix.js

--- src/YASMIJ.Matrix.js
+++ src/YASMIJ.Matrix.js
@@ -2,19 +2,35 @@
 * @project YASMIJ.js, "Yet another simplex method implementation in Javascript"
 * @author Larry Battle
 * @license MIT License <http://www.opensource.org/licenses/mit-license>
-* @date 07/02/2012
 */
 (function(root){
 	"use strict";
 	// Matrix Class
+	/**
+	* Represents a matrix.
+	* @constructor YASMIJ.Matrix
+	*/
 	var Matrix = function(){
 		this.array = [];
-		this.rows = 0;
-		this.columns = 0;
 	};
+	/**
+	* Checks if an object is an array.
+	* @param {Object}
+	* @return {Boolean}
+	* @example
+	Matrix.isArray({}) === false;
+	Matrix.isArray([1,2]) === true;
+	*/
 	Matrix.isArray = function( obj ){
 		return Object.prototype.toString.call(obj) === "[object Array]";
 	};
+	/**
+	* Converts an array to an matrix
+	* @param {Array} input - single or two dimensional array
+	* @return {Matrix}
+	* @example
+	YASMIJ.Matrix.parse([[1,2,3],[1,2,3]]).toString() === "[[1,2,3],[1,2,3]]";
+	*/
 	Matrix.parse = function( input ){
 		var obj = new Matrix(), isArray = Matrix.isArray( input );
 		if( input !== undefined && !( isArray && !input.length) ){	
@@ -27,6 +43,14 @@
 		}
 		return obj;
 	};
+	/**
+	* Returns a copy of a single dimensional array multiplied by factor
+	* @param {Number} scale - constant value
+	* @param {Array} row - single dimensional array 
+	* @return {Array}
+	* @example
+	YASMIJ.Matrix.scaleRow(3, [1,2,3] ); // returns [3,6,9]
+	*/
 	Matrix.scaleRow = function(scale, row){
 		if( !Matrix.isArray(row) ){
 			return row;
@@ -37,9 +61,28 @@
 		}
 		return row;
 	};
+	/**
+	* Adds two single dimensional arrays together
+	* @param {Array} rowA - single dimensional
+	* @param {Array} rowB - single dimensional
+	* @return {Array} rowA + rowB
+	* @example
+	YASMIJ.Matrix.addRows( [1,2], [2,3] ); // returns [3,5]
+	*/
 	Matrix.addRows = function(rowA, rowB){
 		return Matrix.scaleAndAddRows(1, rowA, 1, rowB );
 	};
+	/**
+	* Scales two single dimensional arrays then adds them together.
+	* Same as ((scaleA * rowA) + (scaleB * rowB))
+	* @param {Number} scaleA - constant
+	* @param {Array} rowA - single dimensional
+	* @param {Number} scaleB - constant
+	* @param {Array} rowB - single dimensional
+	* @return {Array}
+	* @example 
+	YASMIJ.Matrix.scaleAndAddRows( 2, [1,2], 3, [4,5] ); // returns [14, 19]
+	*/
 	Matrix.scaleAndAddRows = function( scaleA, rowA, scaleB, rowB){
 		rowA = (Matrix.isArray(rowA)) ? rowA.concat() : [];
 		rowB = (Matrix.isArray(rowB)) ? rowB.concat() : [];
@@ -49,6 +92,13 @@
 		}
 		return rowA;
 	};
+	/**
+	* Negates all elements in a single dimensional array.
+	* @param {Array} - single dimensional array
+	* @return {Array}
+	* @example 
+	YASMIJ.Matrix.inverseArray([-1,0,3]); // returns [1,0,-3]
+	*/
 	Matrix.inverseArray = function( arr ){
 		if( !Matrix.isArray( arr ) ){
 			return arr;
@@ -59,14 +109,32 @@
 		}
 		return arr;
 	};
+	/**
+	* Adds a new single dimensional array 
+	* @param {Array} - single dimensional array
+	* @return {Matrix} self
+	* @chainable
+	* @example
+	
+	*/
 	Matrix.prototype.addRow = function(arr){
 		arr = Matrix.isArray(arr) ? arr : [arr];
 		this.array.push( arr );
 		return this;
 	};
+	/**
+	* 
+	* @param {}
+	* @return {}
+	*/
 	Matrix.prototype.getElement = function( i, j ){
 		return this.array[i][j];
 	};
+	/**
+	* 
+	* @param {}
+	* @return {}
+	*/
 	Matrix.prototype.getColumn = function( j ){
 		var arr = [];
 		this.forEachRow(function(i, row){
@@ -74,9 +142,19 @@
 		});
 		return arr;
 	};
+	/**
+	* 
+	* @param {}
+	* @return {}
+	*/
 	Matrix.prototype.getRow = function( i ){
 		return this.array[i];
 	};
+	/**
+	* 
+	* @param {}
+	* @return {}
+	*/
 	Matrix.prototype.get = function( row, col ){
 		if( !col && col !== 0 ){
 			return this.getRow( row );
@@ -86,6 +164,11 @@
 			return this.getColumn( col );
 		}
 	};
+	/**
+	* 
+	* @param {}
+	* @return {}
+	*/
 	Matrix.prototype.forEachRow = function(fn){
 		var newRow;
 		if( typeof fn === "function"){
@@ -119,10 +202,20 @@
 		}
 		return arr;
 	};
+	/**
+	* 
+	* @param {}
+	* @return {}
+	*/
 	Matrix.prototype.getMostNegIndexFromLastRow = function(){
 		var result = this.getMinElementInRow( this.array.length - 1, true );
 		return result[1] < 0 ? result[0] : -1;
 	};
+	/**
+	* 
+	* @param {}
+	* @return {}
+	*/
 	Matrix.prototype.getRowIndexWithPosMinColumnRatio = function( colI ){
 		var rowI = -1, minVal = Infinity, i = 0, len = this.array.length - 1, val, arr;
 		if( colI < 0 || this.array[0].length <= colI ){
@@ -138,6 +231,11 @@
 		}
 		return rowI;
 	};
+	/**
+	* 
+	* @param {}
+	* @return {}
+	*/
 	Matrix.prototype.toString = function(){
 		var str = "";
 		this.forEachRow( function( i, row ){
@@ -163,6 +261,11 @@
 		}
 		return [ columns, rows ];
 	};
+	/**
+	* 
+	* @param {}
+	* @return {}
+	*/
 	Matrix.prototype.scaleRow = function(scaleA, rowI ){
 		var row = this.array[ rowI ] || [];
 		for(var i = 0, len = row.length; i < len; i++ ){
@@ -170,6 +273,11 @@
 		}
 		return this;
 	};
+	/**
+	* 
+	* @param {}
+	* @return {}
+	*/
 	Matrix.prototype.addToRow = function(iRow, els){
 		if( this.array[iRow] ){
 			this.array[iRow] = (this.array[iRow] || []).concat(els);
@@ -196,6 +304,11 @@
 		}
 		return [ index, max ];
 	};
+	/**
+	* 
+	* @param {}
+	* @return {}
+	*/
 	Matrix.prototype.pivot = function( rowI, colI ){
 		var i = this.array.length, val, pRow = this.array[ rowI ];
 		if( !pRow ){
@@ -211,6 +324,11 @@
 		}
 		return this;
 	};
+	/**
+	* 
+	* @param {}
+	* @return {}
+	*/
 	Matrix.prototype.getUnitValueForColumn = function( colI ){
 		var nonZeroValues = 0, val = 0;
 		
@@ -225,6 +343,11 @@
 		val = ( nonZeroValues === 1 ) ? val : 0; 
 		return val;
 	};
+	/**
+	* 
+	* @param {}
+	* @return {}
+	*/
 	Matrix.prototype.getLastElementOnLastRow = function(){
 		var row = this.array[this.array.length - 1];
 		return row[ row.length - 1];
