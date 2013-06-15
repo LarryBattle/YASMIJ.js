138c6725965b89ba51df71ac75ef4d4f7063dbda
Larry Battle <larry.battle.tech@gmail.com>
2013-06-15 12:06:31 -0500

    Updated the demo

--- js/demo.js
+++ js/demo.js
@@ -1,20 +1,27 @@
-var view = {
-  constraint: {},
-	data : {},
+/**
+* Singleton
+*/
+var Funcs = {
+	constraint: {},
+	data : {
+		liveEvents : []
+	},
+	log : {},
 	setup : {},
-	util : {}
+	util : {},
+	view : {}
 };
-view.data.count = 0;
+Funcs.data.count = 0;
 /**
 * Create a DOM element.
 * @param {String} type - type of element to create
 * @param {Object} obj - extends the properties to the DOM element
 * @return {DOM Element}
 */
-view.util.createElement = function(type, obj){
+Funcs.util.createElement = function(type, obj){
 	obj = obj || {};
 	if(obj && typeof obj !== "object"){
-		throw new Error("view.util.createElement() requires an object to be passed.");
+		throw new Error("Funcs.util.createElement() requires an object to be passed.");
 	}
 	var el = document.createElement(type);
 	
@@ -26,80 +33,115 @@ view.util.createElement = function(type, obj){
 	return el;
 };
 /**
-* Returns a DOM element by id.
-* @param {String} id - id of element
-* @return {DOM Element}
-*/
-view.util.getElement = function(id){
-	var el = document.getElementById(id);
-	if(!el){
-		throw new Error("view.util.getElement() was unable to find element with id == `" + id + "`");
-	}
-	return el;
-};
-/**
 * Checks if an element is an object or not. Arrays are objects.
 * @param {Object}
 * @return {Boolean}
 */
-view.util.isObject = function(obj){
+Funcs.util.isObject = function(obj){
 	return obj && typeof obj === "object";
+};
+
+/**
+* Returns a DOM element by id.
+* @param {String} id - id of element
+* @return {DOM Element}
+*/
+Funcs.view.getElement = function(id){
+	var el = document.getElementById(id);
+	if(!el){
+		throw new Error("Funcs.view.getElement() was unable to find element with id == `" + id + "`");
+	}
+	return el;
 };
 /**
 * Checks if an element is Array.
 * @param {Array}
 * @return {Boolean}
 */
-view.util.isArray = function(arr){
+Funcs.util.isArray = function(arr){
 	return Object.prototype.toString.call(arr) === "[object Array]";
 };
-view.util.addElements = function(container, elements){
-	if(!view.util.isObject(container) || !view.util.isObject(elements) ){
-		throw new Error("view.util.addElements(), An object must be passed for both `container` and `elements`.");
+/**
+* Adds array of DOM elements to a parent
+* @param {DOM Element} container
+* @param {Array} elements - array of DOM elements
+* @return {DOM Element}
+*/
+Funcs.util.addElements = function(container, elements){
+	if(!Funcs.util.isObject(container) || !Funcs.util.isObject(elements) ){
+		throw new Error("Funcs.util.addElements(), An object must be passed for both `container` and `elements`.");
 	}
-	elements = view.util.isArray(elements) ? elements : [elements];
+	elements = Funcs.util.isArray(elements) ? elements : [elements];
 	for(var i = 0, len = elements.length; i < len; i++){
 		container.appendChild(elements[i]);
 	}
 	return container;
 };
-view.contraint.add = function(){
-	view.data.count++;
-	var idName = "constraint_" + view.data.count;
-	var container = view.util.getElement("constraintArea");
-	view.util.addElements(
+// add new constraint block
+Funcs.view.addConstraintBlock = function(){
+	Funcs.data.count++;
+	var idName = "constraint_" + Funcs.data.count;
+	var container = Funcs.view.getElement("constraintArea");
+	var constraintBlock = Funcs.view.createElement("div", {
+		"id" : "div_"+idName
+	});
+	
+	Funcs.util.addElements(
 		container,
 		[
-			view.util.createElement("input", {
+			Funcs.util.createElement("input", {
 				"type" : "text",
-				"id" : idName
+				"id" : "input_" + idName
 			}),
-			view.util.createElement("input", {
+			Funcs.util.createElement("input", {
 				"type" : "button",
 				"class" : "deleteConstraint",
-				"for" : idName,
+				"for" : "delete_"+idName,
 				"value" : "Delete"
 			}),
-			view.util.createElement("br")
-		]
+			Funcs.util.createElement("br")
+		]	
 	);
 };
-view.util.addLiveEvent = function(){
-	var events = [];
+// add events to document.onclick()
+Funcs.view.addLiveEvent = function(fn){
+	if(typeof fn !== "function" ){
+		return;
+	}
+	var events = Funcs.data.liveEvents;
+	events.push(fn);
 	document.onclick = function(event) {
+		console.log(event);
+		console.log(events);
+		return;
 		for(var i = 0, len = events.length; i < len; i++ ){
 			events[i](event);
 		}
 	};
 };
-view.logs.addMessage = function(msg){
-	var logs = view.util.getElement("constraintArea");
+// add message to the log
+Funcs.log.addMessage = function(msg){
+	var logs = Funcs.view.getElement("constraintArea");
 	logs.innerHTML += "\r\n"+msg;
 };
-view.setup.addEvents = function(){
-	
+// setup all the events
+Funcs.setup.addEvents = function(){
+	Funcs.view.addLiveEvent(function(e){
+		if(!e || !e.target || e.target.className !== "" ){
+			return;
+		}
+		var el = e.target;
+		var id = "#" + el["for"];
+		Funcs.view.getElement("constraintArea").removeChild(
+			Funcs.view.getElement(id)
+		);
+	});
+	Funcs.view.getElement("addConstraint").onclick = function(){
+		Funcs.view.addConstraintBlock(); 
+	};
 };
-view.setup.init = function(){
-	view.setup.addEvents();
+// Startup function
+Funcs.setup.init = function(){
+	Funcs.setup.addEvents();
 };
-view.setup.init();
+Funcs.setup.init();
